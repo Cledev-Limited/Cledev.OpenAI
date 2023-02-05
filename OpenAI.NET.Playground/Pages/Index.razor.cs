1d6dee917e256d6434b16561b9426e26b0ea2bba
lucabriguglia <luca.briguglia@outlook.com>
2023-02-05 15:50:48 +0000

    Update playground app

---
+++ OpenAI.NET.Playground/Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using OpenAI.NET.SDK.V1;
+
+namespace OpenAI.NET.Playground.Pages
+{
+    public class IndexPage : ComponentBase
+    {
+        [Inject] public IOpenAIService OpenAIService { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var test = await OpenAIService.CreateCompletion(CompletionsModel.Ada, prompt: "Say something nice");
+        }
+    }
+}
--- OpenAI.NET.Playground/Program.cs
+++ OpenAI.NET.Playground/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OpenAI.NET.Playground;
+using OpenAI.NET.SDK.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOpenAIService();
 
 await builder.Build().RunAsync();
