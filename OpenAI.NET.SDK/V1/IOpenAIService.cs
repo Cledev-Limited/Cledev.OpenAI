a23d7db4faae3f42b2e1fde75b584af2c8988d90
lucabriguglia <luca.briguglia@outlook.com>
2023-02-05 14:00:40 +0000

    Move service interface to a new file and add XML comments

---
+++ OpenAI.NET.SDK/V1/IOpenAIService.cs
@@ -0,0 +1,86 @@
+﻿using OpenAI.NET.SDK.V1.Contracts;
+
+namespace OpenAI.NET.SDK.V1;
+
+public interface IOpenAIService
+{
+    /// <summary>
+    /// Lists the currently available models, and provides basic information about each one such as the owner and availability.
+    /// </summary>
+    Task<RetrieveModelsResponse?> RetrieveModels();
+
+    /// <summary>
+    /// Retrieves a model instance, providing basic information about the model such as the owner and permissioning.
+    /// </summary>
+    /// <param name="id">The id of the model (e.g. text-davinci-003)</param>
+    Task<RetrieveModelsResponse.RetrieveModelsResponseData?> RetrieveModel(string id);
+
+    /// <summary>
+    /// Creates a completion for the provided prompt and parameters.
+    /// </summary>
+    /// <param name="request">The create completion request.</param>
+    /// <returns>The create completion response.</returns>
+    Task<CreateCompletionResponse?> CreateCompletion(CreateCompletionRequest request);
+
+    /// <summary>
+    /// Creates a completion for the provided prompt and parameters.
+    /// </summary>
+    /// <param name="model">ID of the model to use (e.g. text-davinci-003).</param>
+    /// <param name="prompt">
+    /// <para>The prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays.</para>
+    /// <para>Note that &amp;lt;|endoftext|&amp;gt; is the document separator that the model sees during training, so if a prompt is not specified the model will generate as if from the beginning of a new document.</para>
+    /// </param>
+    /// <param name="maxTokens">
+    /// <para>The maximum number of tokens to generate in the completion.</para>
+    /// <para>The token count of your prompt plus max_tokens cannot exceed the model's context length. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).</para>
+    /// </param>
+    /// <returns>The create completion response.</returns>
+    Task<CreateCompletionResponse?> CreateCompletion(CompletionsModel model, string? prompt = null, int? maxTokens = null);
+
+    /// <summary>
+    /// Creates a new edit for the provided input, instruction, and parameters.
+    /// </summary>
+    /// <param name="request">The create edit request.</param>
+    /// <returns>The create edit response</returns>
+    Task<CreateEditResponse?> CreateEdit(CreateEditRequest request);
+
+    /// <summary>
+    /// Creates a new edit for the provided input, instruction, and parameters.
+    /// </summary>
+    /// <param name="model">ID of the model to use (e.g. text-davinci-edit-001).</param>
+    /// <param name="input">The input text to use as a starting point for the edit.</param>
+    /// <param name="instruction">The instruction that tells the model how to edit the prompt.</param>
+    /// <returns></returns>
+    Task<CreateEditResponse?> CreateEdit(EditsModel model, string? input = null, string? instruction = null);
+
+    /// <summary>
+    /// Creates an image given a prompt.
+    /// </summary>
+    /// <param name="request">The create image request.</param>
+    /// <returns>The create image response.</returns>
+    Task<CreateImageResponse?> CreateImage(CreateImageRequest request);
+
+    /// <summary>
+    /// Creates an image given a prompt.
+    /// </summary>
+    /// <param name="prompt">A text description of the desired image(s). The maximum length is 1000 characters.</param>
+    /// <param name="numberOfImagesToGenerate">The number of images to generate. Must be between 1 and 10.</param>
+    /// <param name="size">The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.</param>
+    /// <param name="responseFormat">The format in which the generated images are returned. Must be one of url or b64_json.</param>
+    /// <returns>The create image response.</returns>
+    Task<CreateImageResponse?> CreateImage(string prompt, int? numberOfImagesToGenerate = null, ImageSize? size = null, ImageResponseFormat? responseFormat = null);
+
+    /// <summary>
+    /// Creates an edited or extended image given an original image and a prompt.
+    /// </summary>
+    /// <param name="request">The create image edit request.</param>
+    /// <returns>The create image response.</returns>
+    Task<CreateImageResponse?> CreateImageEdit(CreateImageEditRequest request);
+
+    /// <summary>
+    /// Creates a variation of a given image.
+    /// </summary>
+    /// <param name="request">The create image variation request.</param>
+    /// <returns>The create image response.</returns>
+    Task<CreateImageResponse?> CreateImageVariation(CreateImageVariationRequest request);
+}
--- OpenAI.NET.SDK/V1/OpenAIService.cs
+++ OpenAI.NET.SDK/V1/OpenAIService.cs
@@ -6,20 +6,6 @@ using OpenAI.NET.SDK.V1.Contracts;
 
 namespace OpenAI.NET.SDK.V1;
 
-public interface IOpenAIService
-{
-    Task<RetrieveModelsResponse?> RetrieveModels();
-    Task<RetrieveModelsResponse.RetrieveModelsResponseData?> RetrieveModel(string id);
-    Task<CreateCompletionResponse?> CreateCompletion(CreateCompletionRequest request);
-    Task<CreateCompletionResponse?> CreateCompletion(string prompt, CompletionsModel? model = null, int? maxTokens = null);
-    Task<CreateEditResponse?> CreateEdit(CreateEditRequest request);
-    Task<CreateEditResponse?> CreateEdit(string input, string instruction, EditsModel? model = null);
-    Task<CreateImageResponse?> CreateImage(CreateImageRequest request);
-    Task<CreateImageResponse?> CreateImage(string prompt, int? numberOfImagesToGenerate = null, ImageSize? size = null, ImageResponseFormat? responseFormat = null);
-    Task<CreateImageResponse?> CreateImageEdit(CreateImageEditRequest request);
-    Task<CreateImageResponse?> CreateImageVariation(CreateImageVariationRequest request);
-}
-
 public class OpenAIService : IOpenAIService
 {
     private const string ApiVersion = "v1";
@@ -37,48 +23,55 @@ public class OpenAIService : IOpenAIService
         }
     }
 
+    /// <inheritdoc />
     public async Task<RetrieveModelsResponse?> RetrieveModels()
     {
         return await _httpClient.GetFromJsonAsync<RetrieveModelsResponse?>($"/{ApiVersion}/models");
     }
 
+    /// <inheritdoc />
     public async Task<RetrieveModelsResponse.RetrieveModelsResponseData?> RetrieveModel(string id)
     {
         return await _httpClient.GetFromJsonAsync<RetrieveModelsResponse.RetrieveModelsResponseData?>($"/{ApiVersion}/models/{id}");
     }
 
+    /// <inheritdoc />
     public async Task<CreateCompletionResponse?> CreateCompletion(CreateCompletionRequest request)
     {
         var response = await _httpClient.PostAsJsonAsync($"/{ApiVersion}/completions", request);
         return await response.Content.ReadFromJsonAsync<CreateCompletionResponse?>();
     }
 
-    public async Task<CreateCompletionResponse?> CreateCompletion(string prompt, CompletionsModel? model = null, int? maxTokens = null)
+    /// <inheritdoc />
+    public async Task<CreateCompletionResponse?> CreateCompletion(CompletionsModel model, string? prompt = null, int? maxTokens = null)
     {
         return await CreateCompletion(new CreateCompletionRequest
         {
-            Model = (model ?? CompletionsModel.Ada).ToStringModel(),
+            Model = model.ToStringModel(),
             Prompt = prompt,
             MaxTokens = maxTokens ?? 16
         });
     }
 
+    /// <inheritdoc />
     public async Task<CreateEditResponse?> CreateEdit(CreateEditRequest request)
     {
         var response = await _httpClient.PostAsJsonAsync($"/{ApiVersion}/edits", request);
         return await response.Content.ReadFromJsonAsync<CreateEditResponse?>();
     }
 
-    public async Task<CreateEditResponse?> CreateEdit(string input, string instruction, EditsModel? model = null)
+    /// <inheritdoc />
+    public async Task<CreateEditResponse?> CreateEdit(EditsModel model, string? input = null, string? instruction = null)
     {
         return await CreateEdit(new CreateEditRequest
         {
-            Model = (model ?? EditsModel.TextDavinciEditV1).ToStringModel(),
+            Model = model.ToStringModel(),
             Input = input,
             Instruction = instruction
         });
     }
 
+    /// <inheritdoc />
     public async Task<CreateImageResponse?> CreateImage(CreateImageRequest request)
     {
         var jsonSerializerOptions = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault };
@@ -86,6 +79,7 @@ public class OpenAIService : IOpenAIService
         return await response.Content.ReadFromJsonAsync<CreateImageResponse?>();
     }
 
+    /// <inheritdoc />
     public async Task<CreateImageResponse?> CreateImage(string prompt, int? numberOfImagesToGenerate = null, ImageSize? size = null, ImageResponseFormat? responseFormat = null)
     {
         return await CreateImage(new CreateImageRequest
@@ -97,6 +91,7 @@ public class OpenAIService : IOpenAIService
         });
     }
 
+    /// <inheritdoc />
     public async Task<CreateImageResponse?> CreateImageEdit(CreateImageEditRequest request)
     {
         var jsonSerializerOptions = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault };
@@ -104,6 +99,7 @@ public class OpenAIService : IOpenAIService
         return await response.Content.ReadFromJsonAsync<CreateImageResponse?>();
     }
 
+    /// <inheritdoc />
     public async Task<CreateImageResponse?> CreateImageVariation(CreateImageVariationRequest request)
     {
         var jsonSerializerOptions = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault };
