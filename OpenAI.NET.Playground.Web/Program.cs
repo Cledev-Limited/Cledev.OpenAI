ccd9cbd2a7770b2b95ddcc4c4ee0250abbc1b7f4
lucabriguglia <luca.briguglia@outlook.com>
2023-02-05 19:18:53 +0000

    Update blazor app

--- OpenAI.NET.Playground.Web/Program.cs
+++ OpenAI.NET.Playground.Web/Program.cs
@@ -1,11 +1,27 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OpenAI.NET.Playground.Web;
+using OpenAI.NET.SDK;
+using OpenAI.NET.SDK.Extensions;
+using OpenAI.NET.SDK.V1;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.openai.com/") });
+
+builder.Services.AddOptions<OpenAISettings>().Configure(options =>
+{
+    options.ApiKey = builder.Configuration["OpenAI:ApiKey"];
+    options.Organization = builder.Configuration["OpenAI:Organization"];
+});
+builder.Services.AddHttpClient<IOpenAIService, OpenAIService>();
+
+//builder.Services.AddOpenAIService(options =>
+//{
+//    options.ApiKey = builder.Configuration["OpenAI:ApiKey"];
+//    options.ApiKey = builder.Configuration["OpenAI:Organization"];
+//});
 
 await builder.Build().RunAsync();
