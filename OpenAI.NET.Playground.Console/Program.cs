1bd5cec8571cfabc971912afef949862e487c3ab
lucabriguglia <luca.briguglia@outlook.com>
2023-02-06 09:23:13 +0000

    Rename service to client

--- OpenAI.NET.Playground.Console/Program.cs
+++ OpenAI.NET.Playground.Console/Program.cs
@@ -22,14 +22,14 @@ serviceCollection.AddScoped(_ => configuration);
 serviceCollection.AddOpenAIClient();
 
 var serviceProvider = serviceCollection.BuildServiceProvider();
-var service = serviceProvider.GetRequiredService<IOpenAIClient>();
+var client = serviceProvider.GetRequiredService<IOpenAIClient>();
 
-//var response = await service.RetrieveModels();
-//var response = await service.RetrieveModel("text-davinci-003");
-var response = await service.CreateCompletion(CompletionsModel.Ada, "Say something nice");
-//var response = await service.CreateCompletion("What is MOB programming?", OpenAIModels.TextDavinciV3, maxTokens: 100);
-//var response = await service.CreateEdit(input: "What day of the wek is it?", instruction: "Fix the spelling mistakes");
-//var response = await service.CreateImage(prompt: "A cute baby sea otter");
+//var response = await client.RetrieveModels();
+//var response = await client.RetrieveModel("text-davinci-003");
+var response = await client.CreateCompletion(CompletionsModel.Ada, "Say something nice");
+//var response = await client.CreateCompletion("What is MOB programming?", OpenAIModels.TextDavinciV3, maxTokens: 100);
+//var response = await client.CreateEdit(input: "What day of the wek is it?", instruction: "Fix the spelling mistakes");
+//var response = await client.CreateImage(prompt: "A cute baby sea otter");
 
 //Console.WriteLine(response.IsSuccess
 //    ? $"{JsonSerializer.Serialize(response.Value, jsonSerializerOptions)}"
