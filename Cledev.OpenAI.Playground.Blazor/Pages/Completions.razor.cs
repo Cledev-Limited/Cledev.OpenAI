e7b199fc77b8d730536c811a6c4cdad3c30ce1ac
lucabriguglia <luca.briguglia@outlook.com>
2023-02-19 12:12:42 +0000

    Add some tooltips to create completion options

--- Cledev.OpenAI.Playground.Blazor/Pages/Completions.razor.cs
+++ Cledev.OpenAI.Playground.Blazor/Pages/Completions.razor.cs
@@ -2,46 +2,50 @@
 using Cledev.OpenAI.V1.Contracts.Completions;
 using Cledev.OpenAI.V1.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Cledev.OpenAI.Playground.Blazor.Pages;
 
 public class CompletionsPage : ComponentBase
 {
     [Inject] public IOpenAIClient OpenAIClient { get; set; } = null!;
+    [Inject] public IJSRuntime JsRuntime { get; set; } = null!;
 
     protected bool IsLoading { get; set; }
 
-    protected CompletionsPageModel Model { get; set; } = null!;
+    protected CreateCompletionRequest Request { get; set; } = null!;
     protected CreateCompletionResponse? Response { get; set; }
+
+    public IList<string> CompletionModels { get; set; } = new List<string>();
 
     protected override void OnInitialized()
     {
-        Model = new CompletionsPageModel
+        Request = new CreateCompletionRequest
         {
-            Request = new CreateCompletionRequest
-            {
-                Model = CompletionsModel.TextDavinciV3.ToStringModel()
-            },
-            CompletionModels = Enum.GetValues(typeof(CompletionsModel)).Cast<CompletionsModel>().Select(x => x.ToStringModel()).ToList()
+            Model = CompletionsModel.TextDavinciV3.ToStringModel()
         };
+
+        CompletionModels = Enum.GetValues(typeof(CompletionsModel)).Cast<CompletionsModel>().Select(x => x.ToStringModel()).ToList();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JsRuntime.InvokeVoidAsync("addTooltips");
+        }
     }
 
     protected async Task OnSubmitAsync()
     {
         IsLoading = true;
         Response = null;
-        Response = await OpenAIClient.CreateCompletion(Model.Request);
+        Response = await OpenAIClient.CreateCompletion(Request);
         IsLoading = false;
 
         if (Response is null)
         {
             // TODO: Display error message
         }
-    }
-
-    protected class CompletionsPageModel
-    {
-        public required CreateCompletionRequest Request { get; set; }
-        public IList<string> CompletionModels { get; set; } = new List<string>();
     }
 }
