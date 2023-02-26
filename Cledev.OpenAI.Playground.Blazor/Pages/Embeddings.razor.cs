81eda463526795cbcdf08a5cda15fbcb60b0c44f
lucabriguglia <luca.briguglia@outlook.com>
2023-02-26 19:17:27 +0000

    Add Embeddings to Blazor Playground

---
+++ Cledev.OpenAI.Playground.Blazor/Pages/Embeddings.razor.cs
@@ -0,0 +1,33 @@
+﻿using Cledev.OpenAI.V1.Contracts.Embeddings;
+using Cledev.OpenAI.V1.Models;
+
+namespace Cledev.OpenAI.Playground.Blazor.Pages;
+
+public class EmbeddingsPage : PageComponentBase
+{
+    protected CreateEmbeddingsRequest Request { get; set; } = null!;
+    protected CreateEmbeddingsResponse? Response { get; set; }
+
+    public IList<string> EmbeddingModels { get; set; } = new List<string>();
+
+    protected override void OnInitialized()
+    {
+        Request = new CreateEmbeddingsRequest
+        {
+            Model = EmbeddingsModel.TextEmbeddingAdaV2.ToStringModel()
+        };
+
+        EmbeddingModels = Enum.GetValues(typeof(EmbeddingsModel)).Cast<EmbeddingsModel>().Select(x => x.ToStringModel()).ToList();
+    }
+
+    protected async Task OnSubmitAsync()
+    {
+        IsLoading = true;
+        Response = null;
+
+        Response = await OpenAIClient.CreateEmbeddings(Request);
+        Error = Response?.Error;
+
+        IsLoading = false;
+    }
+}
--- Cledev.OpenAI/V1/Contracts/Embeddings/CreateEmbeddingsRequest.cs
+++ Cledev.OpenAI/V1/Contracts/Embeddings/CreateEmbeddingsRequest.cs
@@ -12,14 +12,14 @@ public class CreateEmbeddingsRequest
     /// <para>ID of the model to use. You can use the List models API to see all of your available models, or see our Model overview for descriptions of them.</para>
     /// </summary>
     [JsonPropertyName("model")]
-    public required string Model { get; set; }
+    public string Model { get; set; } = null!;
 
     /// <summary>
     /// <para>Required.</para>
     /// <para>Input text to get embeddings for, encoded as a string or array of tokens. To get embeddings for multiple inputs in a single request, pass an array of strings or array of token arrays. Each input must not exceed 8192 tokens in length.</para>
     /// </summary>
     [JsonPropertyName("input")]
-    public required string Input { get; set; }
+    public string Input { get; set; } = null!;
 
     /// <summary>
     /// <para>Optional.</para>
