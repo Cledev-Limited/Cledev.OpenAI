dcc2f5efcf21476b304db48b916d5663d3c283fe
lucabriguglia <luca.briguglia@outlook.com>
2023-03-14 21:29:48 +0000

    Add test project

---
+++ Cledev.OpenAI.Tests/MockHttpClientHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using RichardSzalay.MockHttp;
+
+namespace Cledev.OpenAI.Tests;
+
+public static class MockHttpClientHelpers
+{
+    public static MockHttpMessageHandler GetMockHttpClient()
+    {
+        var mockHttpHandler = new MockHttpMessageHandler();
+        var httpClient = mockHttpHandler.ToHttpClient();
+        httpClient.BaseAddress = new Uri("http://localhost");
+        return mockHttpHandler;
+    }
+
+    public static MockedRequest RespondJson<T>(this MockedRequest request, T content)
+    {
+        request.Respond(req =>
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonSerializer.Serialize(content));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return response;
+        });
+        return request;
+    }
+
+    public static MockedRequest RespondJson<T>(this MockedRequest request, Func<T> contentProvider)
+    {
+        request.Respond(req =>
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonSerializer.Serialize(contentProvider()));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return response;
+        });
+        return request;
+    }
+}
